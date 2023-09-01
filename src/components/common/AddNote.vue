c331d0ebf7986e2fef97797c7f9ca03b1957a5f1
Aniket Aryamane <aryamane.aniket@gmail.com>
2023-09-01 20:01:48 +0530

    Form to add notes

---
+++ src/components/common/AddNote.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue'
+const title = ref('')
+const content = ref('')
+const handleForm = (e) => {
+    console.log('form-submitted')
+}
+</script>
+
+<template>
+    <div class="rs__notes-content">
+        <form @submit.prevent="handleForm">
+            <input
+                type="text"
+                class="rs__input-title"
+                placeholder="Note title..."
+                v-model="title"
+            />
+            <textarea
+                id="content"
+                v-model="content"
+                placeholder="Note Content..."
+            />
+            <button
+                type="submit"
+                class="rs__form-save-btn"
+            >
+                <span class="material-symbols-outlined">save</span>
+            </button>
+        </form>
+    </div>
+</template>
--- src/components/list/ListView.vue
+++ src/components/list/ListView.vue
@@ -1,9 +1,9 @@
 <script setup>
+import AddNote from '@/components/common/AddNote.vue'
 import Sidebar from '@/components/list/Sidebar.vue'
 </script>
 
 <template>
-    <div>
-        <Sidebar />
-    </div>
+    <Sidebar />
+    <AddNote />
 </template>
